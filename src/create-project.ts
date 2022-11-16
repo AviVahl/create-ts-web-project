ba829185d003549a6b81e66e5a4607e0f2ce52f9
Avi Vahl <avi.vahl@wix.com>
2022-11-16 23:31:35 +0200

    refactor: use file URLs to target fs

--- src/create-project.ts
+++ src/create-project.ts
@@ -1,7 +1,14 @@
 import { spawnSync } from "node:child_process";
 import { existsSync } from "node:fs";
-import { copyFile, mkdir, readFile, writeFile } from "node:fs/promises";
-import { join } from "node:path";
+import {
+  copyFile,
+  mkdir,
+  readdir,
+  readFile,
+  writeFile,
+} from "node:fs/promises";
+import { sep } from "node:path";
+import { fileURLToPath, pathToFileURL } from "node:url";
 import {
   amendPackageJson,
   editorConfigContent,
@@ -12,17 +19,23 @@ import {
   tscInitOptions,
 } from "./helpers.js";
 
+const templateURL = new URL(`../template/`, import.meta.url);
+const templateScriptsURL = new URL("scripts/", templateURL);
+
 export async function createProject(projectName: string) {
-  const projectPath = join(process.cwd(), projectName);
-  if (existsSync(projectPath)) {
-    console.error(`${projectPath} already exists!`);
+  const projectURL = new URL(
+    projectName + "/",
+    pathToFileURL(process.cwd() + sep)
+  );
+  if (existsSync(projectURL)) {
+    console.error(`${projectURL} already exists!`);
     return;
   }
-  console.log(`Creating ${projectPath}`);
-  await mkdir(projectPath, { recursive: true });
+  console.log(`Creating ${fileURLToPath(projectURL)}`);
+  await mkdir(projectURL, { recursive: true });
   const spawnOptions = {
     shell: true,
-    cwd: projectPath,
+    cwd: projectURL,
     encoding: "utf8",
   } as const;
 
@@ -57,50 +70,56 @@ export async function createProject(projectName: string) {
   spawnCommand("npx", ["tsc", "--init", ...tscInitOptions]);
 
   console.log(`Amending package.json`);
-  const packageJsonPath = join(projectPath, "package.json");
+  const projectPackageJsonURL = new URL("package.json", projectURL);
   const originalPackageJson = JSON.parse(
-    await readFile(packageJsonPath, "utf8")
+    await readFile(projectPackageJsonURL, "utf8")
   );
   const newPackageJson = amendPackageJson(originalPackageJson);
   await writeFile(
-    packageJsonPath,
+    projectPackageJsonURL,
     JSON.stringify(newPackageJson, null, 2) + "\n"
   );
   console.log(`Reinstalling deps post package.json change`);
   spawnCommand("npm", ["install"]);
 
   console.log(`Creating src folder`);
-  const srcPath = join(projectPath, "src");
-  await mkdir(srcPath);
-  await writeFile(join(srcPath, "main.ts"), srcMainContent);
+  const projectSrcURL = new URL("src/", projectURL);
+  await mkdir(projectSrcURL);
+  await writeFile(new URL("main.ts", projectSrcURL), srcMainContent);
 
   console.log(`Writing .prettierignore`);
-  await writeFile(join(projectPath, ".prettierignore"), prettierIgnoreContent);
+  await writeFile(
+    new URL(".prettierignore", projectURL),
+    prettierIgnoreContent
+  );
 
   console.log(`Writing .npmrc`);
-  await writeFile(join(projectPath, ".npmrc"), npmRcContent);
+  await writeFile(new URL(".npmrc", projectURL), npmRcContent);
 
   console.log(`Creating README`);
-  await writeFile(join(projectPath, "README.md"), generateReadme(projectName));
+  await writeFile(
+    new URL("README.md", projectURL),
+    generateReadme(projectName)
+  );
 
   console.log(`Initializing scripts`);
-  const scriptsPath = join(projectPath, "scripts");
-  await mkdir(scriptsPath);
-  for (const fileName of ["clean.js", "start.js"]) {
+  const projectScriptsURL = new URL("scripts/", projectURL);
+  await mkdir(projectScriptsURL);
+  for (const fileName of await readdir(templateScriptsURL)) {
     await copyFile(
-      new URL(`../template/scripts/${fileName}`, import.meta.url),
-      join(scriptsPath, fileName)
+      new URL(fileName, templateScriptsURL),
+      new URL(fileName, projectScriptsURL)
     );
   }
 
   console.log(`Creating index.html`);
   await copyFile(
-    new URL(`../template/index.html`, import.meta.url),
-    join(projectPath, "index.html")
+    new URL(`index.html`, templateURL),
+    new URL("index.html", projectURL)
   );
 
   console.log(`Creating .editorconfig`);
-  await writeFile(join(projectPath, ".editorconfig"), editorConfigContent);
+  await writeFile(new URL(".editorconfig", projectURL), editorConfigContent);
 
   console.log(`Adding and commiting to git`);
   spawnCommand("git", ["add", "."]);
