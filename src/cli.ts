24cc930745c639396c08e1834ff90cace516af0e
Avi Vahl <avi@vahl.co.il>
2026-07-04 13:33:48 +0300

    feat: print instructions when no project name is provided

--- src/cli.ts
+++ src/cli.ts
@@ -4,4 +4,6 @@ const cliArgs = process.argv.slice(2);
 const [projectName] = cliArgs;
 if (projectName) {
   await createProject(projectName);
+} else {
+  console.error("npm init ts-web-project <project-name>");
 }
